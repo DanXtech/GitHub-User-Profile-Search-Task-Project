7ec2bfbcfdbec6586fd729bf7ab08226c116c912
Adetoye daniel kehinde <150145750+DanXtech@users.noreply.github.com>
2024-10-21 12:15:32 +0100

    Update githubApi.ts

--- utils/githubApi.ts
+++ utils/githubApi.ts
@@ -1,23 +1,23 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const GITHUB_API_BASE_URL = 'https://api.github.com';
+// const GITHUB_API_BASE_URL = 'https://api.github.com';
 
-export const fetchUserProfile = async (username: string) => {
-    try {
-        const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}`);
-        return response.data;
-    } catch (error) {
-        throw new Error('User not found');
-    }
-};
+// export const fetchUserProfile = async (username: string) => {
+//     try {
+//         const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}`);
+//         return response.data;
+//     } catch (error) {
+//         throw new Error('User not found');
+//     }
+// };
 
-export const fetchUserRepos = async (username: string, page: number = 1) => {
-    try {
-        const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}/repos`, {
-            params: { page, per_page: 30 },
-        });
-        return response.data;
-    } catch (error) {
-        throw new Error('Repositories not found');
-    }
-};
+// export const fetchUserRepos = async (username: string, page: number = 1) => {
+//     try {
+//         const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}/repos`, {
+//             params: { page, per_page: 30 },
+//         });
+//         return response.data;
+//     } catch (error) {
+//         throw new Error('Repositories not found');
+//     }
+// };
